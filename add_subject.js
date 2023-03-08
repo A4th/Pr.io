2c78a53c7a5fac9ee5d3cd2be7a1a9fee7d570e2
Jan Paul Batrina <jmbatrina@up.edu.ph>
2023-03-08 22:20:37 +0800

    Add subject: Implement "+" button in grade system

--- add_subject.js
+++ add_subject.js
@@ -1,5 +1,9 @@
 "use strict";
 
+// variable for number of grading system "rows"
+// TODO: use other mechanisms (e.g. closure) instead of global var
+let num_gradeSys = 0;
+
 function inputsFunc() {
     let inputs = [
         "#subName", "#unitNum", "#start", "#end"
@@ -24,3 +28,33 @@ function inputsFunc() {
     }                        
 
 }
+
+function add_gradeSys() {
+    let gradeSysBox = document.getElementById("gradeSysBox");
+
+    // remove add button, to be added to new "row"
+    let add_button = document.getElementById("add_gradeSys");
+    gradeSysBox.removeChild(add_button);
+
+    let gradeSysType = document.createElement("input");
+    gradeSysType.setAttribute("id", `gradeSys-Type-${++num_gradeSys}`);
+    gradeSysType.setAttribute("type", "text");
+    gradeSysType.setAttribute("class", "gradeSys-Type");
+    gradeSysType.setAttribute("placeholder", "e.g. Exam");
+
+    let gradeSysPoints = document.createElement("input");
+    gradeSysPoints.setAttribute("id", `gradeSys-Points-${num_gradeSys}`);
+    gradeSysPoints.setAttribute("type", "number");
+    gradeSysPoints.setAttribute("class", "gradeSys-Points");
+    gradeSysPoints.setAttribute("placeholder", "%, e.g. 30");
+    gradeSysPoints.setAttribute("min", 0);
+    gradeSysPoints.setAttribute("max", 100);
+
+    gradeSysBox.appendChild(gradeSysType);
+    gradeSysBox.appendChild(gradeSysPoints);
+    gradeSysBox.appendChild(add_button);
+    gradeSysBox.appendChild(document.createElement("br"));
+}
+
+
+add_gradeSys();
