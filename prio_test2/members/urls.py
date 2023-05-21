eb303382f3843dc0c18417adb95bea7981fd0f3d
Justin Gabriel Enriquez <jgrenriq@gmail.com>
2023-05-21 23:28:10 +0800

    members app: Add designated registration form for user creation

--- prio_test2/members/urls.py
+++ prio_test2/members/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('login_user/', views.login_user, name="login"),
     path('logout_user', views.logout_user, name="logout"),
+    path('register/', views.register_user, name="register"),
     
 ]
+
+urlpatterns += staticfiles_urlpatterns()
